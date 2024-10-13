f565491f438ecee8b200a674c8457e12915774e3
yuliya <buyul@list.ru>
2024-10-13 15:42:08 +0300

    feat: add admin page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,11 @@ export default function Layout({ children }) {
       <body>
         <header className="p-4 flex gap-4 w-full">
 
-          <Link href="/" className="hover:underline">База служилых людей</Link>
+          {/* <Link href="/" className="hover:underline">База служилых людей</Link>
 
           <Link href="/documents" className="hover:underline">Документы</Link>
 
-          <Link href="/admin" className="hover:underline">Админ</Link>
+          <Link href="/admin" className="hover:underline">Админ</Link> */}
         </header>
 
         {children}</body>
