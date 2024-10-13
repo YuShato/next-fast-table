ba2a130965461726e5c6094ab3603208aebc5446
yuliya <buyul@list.ru>
2024-10-14 02:45:28 +0300

    fix: change ThemeProvider file

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,24 +6,14 @@
 //     return <ThemeProvider attribute="class" defaultTheme='system' enableSystem>{children}</ThemeProvider>
 // }
 
-// import { ThemeProvider } from 'next-themes'
-// import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { ThemeProvider } from 'next-themes'
 
 
-// export function Providers({ children }: { children: React.ReactNode }) {
-//      // @ts-ignore
-//     return (
-//         <ThemeProvider attribute="class" defaultTheme='system' enableSystem>
-//             {children}
-//         </ThemeProvider>
-//     ) as any
-// }
-
-
-
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { type ThemeProviderProps } from 'next-themes/dist/types';
-
-export const ThemeProvider = (props: ThemeProviderProps): React.JSX.Element => {
-    return NextThemesProvider(props) as React.JSX.Element;
-};
+export function Providers({ children }: { children: React.ReactNode }) {
+     // @ts-ignore
+    return (
+        <ThemeProvider attribute="class" defaultTheme='system' enableSystem>
+            {children}
+        </ThemeProvider>
+    ) as any
+}
