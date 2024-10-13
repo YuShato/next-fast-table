fd7532bea687a2c38a1eca1db28d18a44d7fb1b2
yuliya <buyul@list.ru>
2024-10-13 16:45:59 +0300

    feat: add theme switcher

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { Providers } from "./providers";
+import ThemeSwitch from "./components/ThemeSwitch";
 
 
 export default function Layout({ children }) {
   return (
-    <html className="light">
+    <html lang="ru" suppressHydrationWarning>
       <body>
-        <header className="p-4 flex gap-4 w-full">
+        <Providers>
+          <header className="p-4 flex gap-4 w-full">
 
-          {/* <Link href="/" className="hover:underline">База служилых людей</Link>
+            {/* <Link href="/" className="hover:underline">База служилых людей</Link>
 
           <Link href="/documents" className="hover:underline">Документы</Link>
 
           <Link href="/admin" className="hover:underline">Админ</Link> */}
-        </header>
+            <ThemeSwitch />
+          </header>
 
-        {children}</body>
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
