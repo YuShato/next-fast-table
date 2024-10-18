96e3478a75f0786701f14e77f2fe51b8f721b084
yuliya <buyul@list.ru>
2024-10-18 21:15:47 +0300

    fix: fix local storage error

--- package/src/FavoriteIcon.tsx
+++ package/src/FavoriteIcon.tsx
@@ -4,10 +4,82 @@ import { Button } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
 import { toast } from "sonner";
 
+// export function getStorageList() {
+//     const list = localStorage.getItem('dufavorites')
+//     if (list) {
+//         return JSON.parse(list)
+//     } else {
+//         return []
+//     }
+// }
+
+// function FavoriteIconComponent({ favId, favData }: any) {
+//     const [items, setItems] = useState(getStorageList())//
+//     //main helper function to get data from the storage or set it
+
+
+//     //on item change in the list save it to the state and localStorage
+//     useEffect(() => {
+//         localStorage.setItem('dufavorites', JSON.stringify(items))
+//     }, [items])
+//     //checking if the item is already in the list or not
+//     const Favorites = items === null ? false : items.some(item => item.id === favData.id);
+
+//     const handleToggleFavourite = () => {
+//         if (Favorites) {
+//             console.log("remove item")
+//             const currentList = getStorageList()
+//             const removeItem = currentList.find(item => item.id === favData.id);
+//             if (removeItem) {
+//                 const index = currentList.indexOf(removeItem);
+//                 currentList.splice(index, 1);
+//                 setItems(currentList);
+//                 toast.warning('Запись удалена из избранного', {
+//                     position: 'top-center',
+//                     duration: 2000
+//                 });
+//             }
+//         } else {
+//             console.log("add item")
+//             const currentList = getStorageList()
+//             const newList = [...currentList, favData];
+//             setItems(newList);
+//             toast.success('Запись добавлена в избранное', {
+//                 position: 'top-center',
+//                 duration: 2000
+//             });
+//         }
+//     }
+
+//     return (
+//         <>
+//             {Favorites ?
+//                 <Button isIconOnly onClick={handleToggleFavourite}
+//                     variant="flat"
+//                     className='p-1 border-r-2'
+//                 >
+//                     <Icon icon="mdi:heart" color="#CB003D" />
+//                 </Button>
+//                 :
+//                 <Button isIconOnly onClick={handleToggleFavourite}
+//                     className='p-1 border-r-2'
+//                     variant="flat"
+//                 >
+//                     <Icon icon="mdi:heart-outline" color="#CB003D" />
+//                 </Button>
+//             }
+//         </>
+//     )
+// }
+
 export function getStorageList() {
-    const list = localStorage.getItem('dufavorites')
-    if (list) {
-        return JSON.parse(list)
+    if (typeof window !== 'undefined') {
+        const list = localStorage.getItem('dufavorites')
+        if (list) {
+            return JSON.parse(list)
+        } else {
+            return []
+        }
     } else {
         return []
     }
@@ -15,14 +87,18 @@ export function getStorageList() {
 
 function FavoriteIconComponent({ favId, favData }: any) {
     const [items, setItems] = useState(getStorageList())//
-    //main helper function to get data from the storage or set it
+    const [mounted, setMounted] = useState(false)
 
-
-    //on item change in the list save it to the state and localStorage
     useEffect(() => {
-        localStorage.setItem('dufavorites', JSON.stringify(items))
-    }, [items])
-    //checking if the item is already in the list or not
+        setMounted(true)
+    }, [])
+
+    useEffect(() => {
+        if (mounted) {
+            localStorage.setItem('dufavorites', JSON.stringify(items))
+        }
+    }, [items, mounted])
+
     const Favorites = items === null ? false : items.some(item => item.id === favData.id);
 
     const handleToggleFavourite = () => {
