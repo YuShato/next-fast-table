cea36364d57e9c59e4c86e59bbff2d311dc1e8e8
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2026-01-24 23:11:59 +0300

    feat: change prisma config

---
+++ prisma.config.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: { path: 'prisma/migrations' },
+  datasource: {
+    url: env('DATABASE_URL'),
+  },
+})
+
+// import { defineConfig } from '@prisma/config'
+
+// export default defineConfig({
+//   schema: 'prisma/schema.prisma',
+//   datasources: {
+//     db: {
+//       url: process.env.DATABASE_URL!,
+//     },
+//   },
+// })
