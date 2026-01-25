6635a0e31cc573ab14667dc8dc87b9ec175265c3
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2026-01-25 22:20:36 +0300

    feat: upd prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,10 +3,13 @@ import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrations: { path: 'prisma/migrations' },
+  migrations: { path: 'prisma/migrations',
+    seed: 'prisma/seed.js'
+   },
   datasource: {
     url: env('DATABASE_URL'),
   },
+  
 })
 
 // import { defineConfig } from '@prisma/config'
