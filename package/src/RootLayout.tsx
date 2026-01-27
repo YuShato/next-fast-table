33df3318fafd27c9a1e33519380159af4b19c6f1
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2026-01-27 13:51:54 +0300

    test: comment children

--- package/src/RootLayout.tsx
+++ package/src/RootLayout.tsx
@@ -7,13 +7,13 @@ import Footer from "./Footer";
 export default function RootLayout({ children }) {
     return (
         <div className="w-full h-screen relative">
-            {/* <NavHeader /> */}
+            <NavHeader />
             <main style={{ minHeight: "100%" }}>
 
-                {children}
+                {/* {children} */}
             </main>
-{/* 
-            <Footer /> */}
+
+            <Footer />
         </div>
     );
 }
